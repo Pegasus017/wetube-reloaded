366f1d2539c902688f51eddf432bb433537fafc0
Pegasus017 <makh007@naver.com>
2022-11-02 21:00:04 +0900

    middlewares
    from #3.5~#3.10

--- src/server.js
+++ src/server.js
@@ -4,11 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+    console.log(`${req.method}, ${req.url}`)
+    next();
+};
+
 const handleHome = (req, res) => {
-    console.log(res);
     return res.send(`This is Home, http://localhost:${PORT}/`);
 };
 
+app.use(logger);
 app.get("/", handleHome);
 
 const handleListening = () => console.log(`Server is Listening on port http://localhost:${PORT}`);
