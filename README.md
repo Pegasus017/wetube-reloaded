ba00d3988a8624c1d7d3d7843da18f348790a2d8
Pegasus017 <makh007@naver.com>
2022-11-06 03:53:35 +0900

    Routers done
    cleaning code with making routers following basic url rules, and cleaned some middlewares.

---
+++ README.md
@@ -0,0 +1,16 @@
+#Wetube Reloaded
+
+/ -> homepage
+/join -> join
+/login -> login
+/search -> search
+
+/users/:id -> see profile
+/users/logout -> log out
+/users/:id/edit -> edit MY profile
+/users/delete -> delete MY profile
+
+/videos/:id -> watch video
+/videos/:id/edit -> edit video
+/videos/:id/delete -> delete video
+/videos/upload -> upload video
--- src/server.js
+++ src/server.js
@@ -1,21 +1,20 @@
 import express from "express";
+import morgan from "morgan";
+
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
-
-const logger = (req, res, next) => {
-    console.log(`${req.method}, ${req.url}`)
-    next();
-};
-
-const handleHome = (req, res) => {
-    return res.send(`This is Home, http://localhost:${PORT}/`);
-};
-
+const logger = morgan("dev");
 app.use(logger);
-app.get("/", handleHome);
 
-const handleListening = () => console.log(`Server is Listening on port http://localhost:${PORT}`);
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
+
+const handleListening = () => console.log(` Server is Listening on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
