75cc0edf0bfa0b8b428dcba0253e89c13c68056e
Pegasus017 <makh007@naver.com>
2022-11-07 00:20:56 +0900

    organized templates using pug and mvp.css.

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,39 @@
-export const trending = (req,res) => res.send("Homepage Videos");
-export const watch = (req,res) => res.send("Watch Video");
-export const edit = (req,res) => res.send("Edit Video");
+const fakeUser = {
+    username:"Peggy",
+    loggedIn:false,
+}
+
+export const trending = (req,res) => {
+    const videos = [
+        {
+            title: "First video",
+            rating: 5,
+            comments: 2,
+            createdAt: "4 minutes ago",
+            views: 17,
+            id: 1,
+        },
+        {
+            title: "Second video",
+            rating: 5,
+            comments: 2,
+            createdAt: "4 minutes ago",
+            views: 17,
+            id: 2,
+        },
+        {
+            title: "Third video",
+            rating: 5,
+            comments: 2,
+            createdAt: "4 minutes ago",
+            views: 17,
+            id: 3,
+        },
+    ];
+    return res.render("home", {pageTitle:"Home", fakeUser, videos});
+};
+export const watch = (req,res) => res.render("watch");
+export const edit = (req,res) => res.render("edit");
 export const search = (req,res) => res.send("Search Video");
 export const upload = (req,res) => res.send("Upload Video");
 export const deleteVideo = (req,res) => res.send("Delete Video");
